4f5803302e31b15abe8f57a753d48c8899270225
Jason Corbett <jcorbett@expediagroup.com>
2021-05-12 21:04:33 +1000

    Add AristWall

---
+++ src/components/ArtistWall/ArtistWall.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function ArtistWall() {
+  return (
+      <div className="ArtistWall">
+        <div className="ArtistWall__cover"></div>
+        <div className="ArtistWall__slide">
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+          <div className="ArtistWall__item"></div>
+        </div>
+      </div>
+  );
+}
+
+export default ArtistWall;
--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 
 import IntroCta from './IntroCta';
+import ArtistWall from '../ArtistWall/ArtistWall';
 
 import logoStacked from '../../logo-stacked.svg';
 
 function Intro() {
   return (
-    <section className="Intro">
-      <div className="flex flex--guts flex--column">
-        <div className="container">
-          <img src={logoStacked} className="Intro__logo" alt="logo" />
-          <h1 className="Intro__title title title--large">Create and share top five charts</h1>
-          <IntroCta />
+    <div>
+      <ArtistWall />
+      <section className="Intro">
+        <div className="flex flex--guts flex--column">
+          <div className="container">
+            <img src={logoStacked} className="Intro__logo" alt="logo" />
+            <h1 className="Intro__title title title--large">Create and share top five charts</h1>
+            <IntroCta />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
 
