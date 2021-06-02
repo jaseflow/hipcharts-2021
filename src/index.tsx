ee9ac2968b7681cbe8fd611f8ae1f1ce0d29200f
Jason Corbett <jsncbt@gmail.com>
2021-06-02 20:51:45 +1000

    Add react snapshot

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <Router>
       <App />
